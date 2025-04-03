620f731d99cc8bb1e830c35e137a501caee14ce1
shahnawaz <shahnawaz151947@gmail.com>
2025-04-03 18:34:52 +0530

    li visiible

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 // import { Suspense } from "react";
 import "./App.css";
 import ResP from "./components/ResP";
+import { recpCounter } from "./store";
+
 
 function App() {
+    const recps = recpCounter((state) => state.recps);
+  
   return (
     <>
     <ResP/>
+      <div className="flex flex-col gap-4">
+        {recps.map((recp, index) => (
+          <div key={index} >
+            <h1>{recp.title}</h1>
+            <h1>{recp.ingridients}</h1>
+            <h1>{recp.instruction}</h1>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
